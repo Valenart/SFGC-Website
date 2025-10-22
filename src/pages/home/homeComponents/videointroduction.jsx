98ffda1c3a833cfeb5a7029db27ac341110799f3
Richard Valentim da Silva <richardvalentim.corp@gmail.com>
2025-10-21 23:45:24 -0300

    1.2.3.5 - nova tentativa de deploy na Vercel

--- src/pages/home/homeComponents/videointroduction.jsx
+++ src/pages/home/homeComponents/videointroduction.jsx
@@ -33,11 +33,10 @@ export default function VideoIntroduction() {
                 }}
             />
 
-            {/* overlay: cobre exatamente o contêiner do vídeo */}
             <Box
                 sx={{
                     position: 'absolute',
-                    inset: 0, // top:0; right:0; bottom:0; left:0
+                    inset: 0,
                     display: 'flex',
                     justifyContent: 'center',
                     alignItems: 'center',
