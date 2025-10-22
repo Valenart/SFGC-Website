1025573a84e608b211bffe1c26a7bb5cfde39143
Richard Valentim da Silva <richardvalentim.corp@gmail.com>
2025-10-21 21:57:38 -0300

    1.2.3.5 - nova tentativa de deploy na Vercel

--- src/controller/home/home.js
+++ src/controller/home/home.js
@@ -1,5 +1,9 @@
 import { BaseController } from "../baseController/baseController";
 
 export class HomeController extends BaseController {
+    constructor() {
+        super('https://api.example.com/home');
+    }
+
 
 }
